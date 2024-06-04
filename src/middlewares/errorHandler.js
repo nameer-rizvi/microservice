fda27202df85d86e763cc343fd873714e76658d7
nameer-rizvi <rizvinameer@gmail.com>
2024-06-04 09:53:49 -0400

    error handler adjustment

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,13 +1,9 @@
 /* eslint no-unused-vars: 0 */
 function errorHandler(error, req, res, next) {
   res.locals.error = `${error} at ${error.stack.split(" at ")[1].trim()}`; // This gets logged in the logger middleware.
+  if (!error.status) error.status = 500;
   // <<< Create error packet to save in the database here. >>>
-  if (error.status) {
-    // If there is a status provided with the error share it with the client.
-    res.status(error.status).send(error.toString());
-  } else {
-    res.sendStatus(500);
-  }
+  res.status(error.status).send(error.toString()); // Do not share stack trace with client, for security.
 }
 
 module.exports = errorHandler;
